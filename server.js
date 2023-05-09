c0b4cf6e2516d8f01a226e5211f9cada6337c396
David Osei-Tutu <97769558+ohsay22@users.noreply.github.com>
2023-05-09 04:55:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -71,6 +71,44 @@ app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:htt
 // Serve static files
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
 app.use('/', express.static(staticpath))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/app', function(req, res) {
+    res.status(200).send("200 OK");
+});
+app.get('/app/rps', function(req, res) {
+    res.status(200).send(rps());
+});
+app.get('/app/rpsls', function(req, res) {
+    res.status(200).send(rpsls());
+});
+
+app.get('/app/rps/play', function(req, res) {
+    res.status(200).send(rps(req.query.shot));
+});
+app.get('/app/rpsls/play', function(req, res) {
+    res.status(200).send(rpsls(req.query.shot));
+});
+app.post('/app/rps/play', function(req, res) {
+    res.status(200).send(rps(req.body.shot));
+});
+app.post('/app/rpsls/play', function(req, res) {
+    res.status(200).send(rpsls(req.body.shot));
+});
+
+app.get('/app/rps/play/:shot', function(req, res) {
+    res.status(200).send(rps(req.params.shot));
+});
+app.get('/app/rpsls/play/:shot', function(req, res) {
+    res.status(200).send(rpsls(req.params.shot));
+});
+
+app.get('*', function(req, res){
+      res.status(404).send('404 NOT FOUND');
+});
+
 // Create app listener
 const server = app.listen(port)
 // Create a log entry on start
